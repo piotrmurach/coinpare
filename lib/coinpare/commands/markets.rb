303e39120da5e531b50947421b6693946ffd6929
Piotr Murach <pmurach@gmail.com>
2018-05-16 20:29:38 +0200

    Add ability to watch for market changes

--- lib/coinpare/commands/markets.rb
+++ lib/coinpare/commands/markets.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 require 'pastel'
+require 'tty-pager'
 require 'tty-spinner'
 require 'tty-table'
+require 'timers'
 
 require_relative '../command'
 require_relative '../fetcher'
@@ -14,30 +16,60 @@ module Coinpare
         @name = name
         @options = options
         @pastel = Pastel.new
+        @timers = Timers::Group.new
         @spinner = TTY::Spinner.new(':spinner Fetching data...',
                                     format: :dots, clear: true)
       end
 
       def execute(input: $stdin, output: $stdout)
+        pager = TTY::Pager::BasicPager.new(output: output)
         @spinner.auto_spin
 
+        if @options['watch']
+          output.print cursor.hide
+          interval = @options['watch'].to_f > 0 ? @options['watch'].to_f : DEFAULT_INTERVAL
+          @timers.now_and_every(interval) { display_markets(output, pager) }
+          loop { @timers.wait }
+        else
+          display_markets(output, pager)
+        end
+      ensure
+        @spinner.stop
+        if @options['watch']
+          @timers.cancel
+          output.print cursor.clear_screen_down
+          output.print cursor.show
+        end
+      end
+
+      def display_markets(output, pager)
         to_symbol = fetch_symbol
         response = Fetcher.fetch_top_exchanges_by_pair(
                      @name.upcase, @options['base'].upcase, @options)
-
+        return unless response
         table = setup_table(response["Data"]["Exchanges"], to_symbol)
 
+        lines = banner.lines.size + 1 + table.rows_size + 3
         @spinner.stop
+        clear_output(output, lines) { print_results(table, output, pager) }
+      end
 
+      def clear_output(output, lines)
+        output.print cursor.clear_screen_down if @options['watch']
+        yield if block_given?
+        output.print cursor.up(lines) if @options['watch']
+      end
+
+      def print_results(table, output, pager)
         output.puts banner
-        output.puts table.render(:unicode, padding: [0, 1], alignment: :right)
-      ensure
-        @spinner.stop
+        pager.page(table.render(:unicode, padding: [0, 1], alignment: :right))
+        output.puts
       end
 
       def fetch_symbol
         prices = Fetcher.fetch_prices(
                    @name.upcase, @options['base'].upcase, @options)
+        return unless prices
         prices['DISPLAY'][@name.upcase][@options['base'].upcase]['TOSYMBOL']
       end
 
